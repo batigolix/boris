1267925824142db76b20aed4dc71bbfadcd8cc46
Boris Doesborg <bdoesborg@gmail.com>
2016-04-14 11:44:06 +0200

    wip ec map

---
+++ docroot/data.js
@@ -0,0 +1,339 @@
+var data = {
+  "AT": {
+    "label": "Austria",
+    "projects": {
+      "0" : {
+        "name": "Produktion der Zukunft (Production of the Future)",
+        "url" : "url": "http://www.ffg.at/produktion",
+        "type": "national"
+      }   
+    }
+  },
+  "BE": {
+    "label": "Belgium",
+    "projects": {
+      "0" : {
+        "name": "MADE DIFFERENT – Factories of the future",
+        "url": "http://www.madedifferent.be",
+        "type": "national"
+      },
+      "1": {
+        "name": "Marshall 4.0",
+        "url": "http://www.wallonie.be/fr/plan-marshall",
+         "type": "national"
+      }   
+      "2": {
+        "name": "Flanders Make ",
+        "url": "http://www.flandersmake.be",
+        "type": "national"
+      }
+      "3": {
+        "name": "iMinds",
+        "url": "http://www.iminds.be",
+        "type": "national"
+      }
+    }
+  },
+  "CZ": {
+    "label": "Czech Republic",
+    "projects": {
+      "0": {
+        "name": "Průmysl 4.0/ Industry 4.0",
+        "url": "http://www.mpo.cz/dokument162351.html",
+        "type": "national"
+      }
+    }
+  },
+  "DK": {
+    "label": "Denmark",
+    "projects": {
+      "0": {
+        "name": "MADE – Manufacturing Academy of Denmark",
+        "url": "http://made.dk",
+        "type": "national"
+      }
+    }
+  },
+  "FI": {
+    "label": "Finland",
+    "projects": {
+      "0": {
+        "name": "FIMECC PPP Programmes",
+        "url": "http://www.fimecc.com/programs",
+        "type": "national"
+      },
+      "1" : {
+        "name": "DIGILE  (ICT Alliance with China)",
+        "url": "http://digile.fi/fi",
+        "type": "national"
+      },
+      "2": {
+        "name": "TEKES – Industrial Internet",
+        "url": "http://www.tekes.fi/en/programmes-and-services/tekes-programmes/industrial-internet--business-revolution",
+        "type": "national"
+      }
+    }
+  },
+  "FR": {
+    "label": "France",
+    "projects": {
+      "0": {
+        "name": "Nouvelle France Industrielle",
+        "url": "http://www.economie.gouv.fr/vous-orienter/industrie/nouvelle-france-industrielle",
+        "type": "national"
+      },
+      "1": {
+        "name": "Industrie du Futur",
+        "url": "http://allianceindustrie.wix.com/industrie-dufutur",
+        "type": "national"
+      },
+      "2": {
+        "name": "Transition Numérique",
+        "url": "http://transition-numerique.fr",
+      },
+      "3": {
+        "name": "Le Programme des Investissements d’Avenir",
+        "url": "http://www.agence-nationale-recherche.fr/investissements-d-avenir",
+      },
+      "4": {
+        "name": "FoF Ile-de-France",
+        "url": "http://www.pole-astech.org/site/pages/index.php?doi=859b53568eb0059f7e6cbda05d35a77d",
+      }
+    }
+  },
+  "DE": {
+    "label": "Germany",
+    "projects": {
+      "0": {
+        "name": "Plattform Industrie 4.0",
+        "url": "http://www.plattform-i40.de",
+      },
+      "1": {
+        "name": "Mittelstand 4.0",
+        "url": "http://www.mittelstand-digital.de/DE/Foerderinitiativen/mittelstand-4-0.html",
+      },
+      "2": {
+        "name": "Smart Service World ",
+        "url": "http://industrie4.0.gtai.de/INDUSTRIE40/Navigation/EN/Topics/smart-service-world.html",
+      },
+      "3": {
+        "name": "Autonomik for Industrie 4.0 - Production, Products, Services in the Internet of the Future",
+        "url": "http://autonomik40.de",
+      },
+      "4": {
+        "name": "It's OWL",
+        "url": "http://www.its-owl.com/home",
+      },
+      "5": {
+        "name": "Allianz Industrie 4.0 Baden-Würtemberg",
+        "url": "http://www.i40-bw.de",
+      }
+    }
+  },
+  "EL": {
+    "label": "Greece",
+    "projects": {
+      "0": {
+        "name": "Regional Operational Programme of Region of Western Greece for the Period 2014-2020",
+        "url": "http://dytikiellada.gr",
+      }
+    }
+  },
+  "IT": {
+    "label": "Italy",
+    "projects": {
+      "0": {
+        "name": "Internet of Things and Industry 4.0",
+        "url": "",
+      },
+      "1": {
+        "name": "Cluster Fabbrica Intelligente CFI (Intelligent Factories Cluster)",
+        "url": "http://www.fabbricaintelligente.it/",
+      },
+      "2": {
+        "name": "Cluster Fabbrica Intelligente Lombardia AFIL (Cluster Intelligent Factories Lombardy)",
+        "url": "http://www.afil.it",
+      },
+    }
+  },
+  "LT": {
+    "label": "Latvia",
+    "projects": {
+      "0": {
+        "name": "Demola Latvia (Riga IT TechHub)",
+        "url": "http://latvia.demola.net/about",
+      }
+    }
+  },
+  "NL": {
+    "label": "Netherlands",
+    "projects": {
+      "0": {
+        "name": "Smart Industry",
+        "url": "http://www.smartindustry.nl",
+      }
+    }
+  },
+  "CODE": {
+    "label": "Poland",
+    "projects": {
+      "0": {
+        "name": "INNOMED",
+        "url": "http://www.ncbir.pl/programy-krajowe/programy-sektorowe/innomed",
+      },
+      "1": {
+        "name": "INNOLOT",
+        "url": "http://www.ncbir.pl/programy-krajowe/programy-sektorowe/innolot",
+      },
+      "2": {
+        "name": "CuBR",
+        "url": "http://www.ncbir.pl/en/news/art,3875,more-than-pln-100-million-in-3rd-competition-for-innovators-in-non-ferrous-metals-industry.html",
+      },
+      "3": {
+        "name": "BIOSTRATEG",
+        "url": "http://www.ncbr.gov.pl/programy-strategiczne/srodowisko-naturalne-rolnictwo-i-lesnictwo---biostrateg",
+      }
+    }
+  }
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+",
+",
+    "label": "Portugal",
+        "name": "PRODUTECH – Production Technologies Cluster",
+"www.produtech.org",
+"url": "http://mobilizadores.produtech.org/en",
+"url": "http://portal.produtech.org/",
+",
+    "label": "Slovakia",
+"Smart Industry",
+",
+"",
+    "label": "Spain",
+"Industria Conectada 4.0",
+"url": "http://www.industriaconectada40.gob.es",
+",
+"Spain/ Basque Country",
+        "name": "Basque Industry 4.0",
+"www.spri.eus/industry40",
+",
+    "label": "Sweden",
+        "name": "Produktion 2030",
+"url": "http://www.produktion2030.se/",
+",
+    "label": "United Kingdom",
+        "name": "High value Manufacturing Catapult",
+"url": "shttp://hvm.catapult.org.uk/",
+",
+"United Kingdom",
+        "name": "Innovate UK",
+"url": "shttp://www.gov.uk/government/organisations/innovate-uk",
+",
+"United Kingdom",
+        "name": "EPSRC Manufacturing the Future theme",
+"url": "shttp://www.epsrc.ac.uk/research/ourportfolio/themes/manufacturingthefuture/",
+",
+"United Kingdom - Scotland",
+        "name": "Action Plan for Manufacturing",
+"url": "http://www.scottish-enterprise.com/knowledge-hub/articles/insight/scotlands-manufacturing-action-plan",
+",
+",
+"
+
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+    "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
+
+  "CODE": {
+    "label": "",
+    "projects": {
+      "0": {
+        "name": "",
+        "url": ""
+      }
+    }
+  }
