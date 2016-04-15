a191636d5c7be2f78c822d4101199569674817aa
Boris Doesborg <bdoesborg@gmail.com>
2016-04-15 15:15:02 +0200

    wip ec map

--- docroot/js/fishery.js
+++ docroot/js/fishery.js
@@ -270,7 +270,7 @@ L.custom = {
 		
 		var country_options = { // custom options to defined style and events
 			
-			label : true,
+			label : {mode:"hover"},
 			
 			style: function(feature){
 				var opacity = 1;
